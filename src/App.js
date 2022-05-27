535bd101c38edeb45ef47841d7f49b720f862166
famxuanlinh <famxuanlinh@gmail.com>
2022-05-27 22:01:19 +0700

    Bai 61

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>React app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout || DefaultLayout;
+                        const Page = route.component; //JSX component khi đặt dạng biến thì phải viết hoa chữ cái đầU
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
