213d48a91f0a87d99d08ac36c601d92d7d523c52
famxuanlinh <famxuanlinh@gmail.com>
2022-06-09 21:09:59 +0700

    PropTypes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './Button.module.scss';
@@ -54,5 +54,22 @@ function Button({
         </Comp>
     );
 }
+
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    rounded: PropTypes.bool,
+    text: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
 
 export default Button;
--- src/components/Popper/Menu/MenuItems.js
+++ src/components/Popper/Menu/MenuItems.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 
 import classNames from 'classnames/bind';
@@ -14,6 +15,11 @@ const MenuItems = ({ data, onClick }) => {
             {data.title}
         </Button>
     );
+};
+
+MenuItems.propTypes = {
+    data: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
 };
 
 export default MenuItems;
